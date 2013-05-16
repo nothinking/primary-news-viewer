4f3ebb2ddcecd097ea281030f4694e4c160de98a
yakulten <yakulten@daumcorp.com>
2013-05-16 10:49:25 +0900

    api 폴더 삭제

--- service/src/routes/index.js
+++ service/src/routes/index.js
@@ -1,5 +1,13 @@
 var index = function(req, res){
-	res.render("index", {});
+	res.render("index", {})
 };
-
 exports.index = index;
+
+// TODO move to config
+var BACKBONE_API_URL = "http://uidev.media.daum.net:3000";
+var DB_NAME = "primarynews";
+
+var api = function(req, res){
+
+}
+exports.api = api;
